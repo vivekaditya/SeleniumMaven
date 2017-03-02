bf9f9af75217a28cb7ce8200f567c76d65bde986
Vivekaditya <vivek.aditya@abzooba.com>
2017-03-02 13:12:49 +0530

    Example project

---
+++ src/main/java/mavendemo/Test.java
@@ -0,0 +1,19 @@
+package mavendemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// Download gecko driver from https://github.com/mozilla/geckodriver/releases
+		// TODO : This should be moved to config file
+		System.setProperty("webdriver.gecko.driver", "/home/abzooba/workspace/automatedtester/logs/geckodriver");
+		WebDriver driver = new FirefoxDriver();
+		String URL = "https://en.wikipedia.org/wiki/Main_Page";
+		driver.navigate().to(URL);
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.quit();
+	}
+}
