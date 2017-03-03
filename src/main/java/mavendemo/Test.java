9435ff89213fcba217e745ac5d45dcee173af5dc
SwgataC <swagata.chatterjee@abzooba.com>
2017-03-03 18:06:37 +0530

    Changes done

--- src/main/java/mavendemo/Test.java
+++ src/main/java/mavendemo/Test.java
@@ -14,42 +14,13 @@ public class Test {
 		// TODO : This should be moved to config file
 		System.setProperty("webdriver.gecko.driver", "C:\\Users\\swagata.chatterjee\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
-//		String URL = "https://en.wikipedia.org/wiki/Main_Page";
-//		driver.navigate().to(URL);
-//		String title = driver.getTitle();
-//		System.out.println(title);
-//		driver.quit();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.navigate().to(" ");
-		driver.manage().window().maximize();
-		int count = 0;
-		for (String line : queryList) {
-			String[] parts = line.split("/t");
-			count = count + 1;
-			if (parts[0].length() != 0) {
-				driver.findElement(By.id("search-field")).clear();
-				driver.findElement(By.id("search-field")).sendKeys(parts[0]);
-				driver.findElement(By.id("btn-search")).click();
-				Thread.sleep(5000);
-				JavascriptExecutor jse = (JavascriptExecutor) driver;
-				do {
-					Thread.sleep(5000);
-					for (int second = 0;; second++) {
-						if (second >= 5) {
-							break;
-						}
-						jse.executeScript("window.scrollBy(0,500)", "");
-						Thread.sleep(1500);
-					}
-					try {
-						driver.findElement(By.linkText("Next »")).click();
-					} catch (Exception ex) {
-						break;
-					}
-				} while (true);
-			}
-		}
-		System.out.println("No. of queries executed: " + count);
+		String URL = "http://54.82.9.134/DomainMapping/index.php";
+		driver.navigate().to(URL);
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.findElement(By.id("Username")).sendKeys("b");
+		driver.findElement(By.id("Password")).sendKeys("a");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
 		driver.close();
 	}
 }
