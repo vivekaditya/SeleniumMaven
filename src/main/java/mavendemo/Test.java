481cce7dfaba26ae5532a6ce2143a42a6e015e4c
unknown <xpresso.support@abzooba.com>
2017-03-03 14:44:51 +0530

    New

--- src/main/java/mavendemo/Test.java
+++ src/main/java/mavendemo/Test.java
@@ -8,7 +8,7 @@ public class Test {
 	public static void main(String[] args) {
 		// Download gecko driver from https://github.com/mozilla/geckodriver/releases
 		// TODO : This should be moved to config file
-		System.setProperty("webdriver.gecko.driver", "/home/abzooba/workspace/automatedtester/logs/geckodriver");
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\swagata.chatterjee\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		String URL = "https://en.wikipedia.org/wiki/Main_Page";
 		driver.navigate().to(URL);
